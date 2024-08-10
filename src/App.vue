c2140d82e0811d4f1ef52655c27a0ae771e7956a
Nemanja Antonijevic <antonijevicnemanja68@gmail.com>
2024-08-10 02:06:20 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <Calculator/>
+  </div>
+</template>
+
+<script setup>
+import Calculator from './components/Calculator.vue';
+</script>
+
+<style scoped>
+
+</style>
